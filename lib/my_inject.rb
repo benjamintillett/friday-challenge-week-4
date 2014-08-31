00755d4b1fe5e67bafd60d2fee3a0c9263d4cb55
benjamintillett <benjamin.tillett@gmail.com>
2014-08-31 08:13:30 +0100

    continue refactoring my_inject

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -2,18 +2,10 @@ class Array
 
 	def my_inject(*args,&block)
 		array = self
-		if !block_given?
-			method =  lambda &args.pop
-			puts method.inspect
-			accumulator = args[0]
-			accumulator = array.slice!(0) unless accumulator
-			array.each { |element| accumulator = method.call(accumulator,element) }
-			accumulator
-		else
-			accumulator = args[0]
-			accumulator = array.slice!(0) unless accumulator
-			array.each { |element| accumulator = block.call(accumulator,element) }
-			accumulator
-		end	
+		block = lambda &args.pop unless block_given?
+		accumulator = args[0]
+		accumulator = array.slice!(0) unless accumulator
+		array.each { |element| accumulator = block.call(accumulator,element) }
+		accumulator
 	end
 end
